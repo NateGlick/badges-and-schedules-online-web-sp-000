f91bb81aa78cd746e9c3c8fad924c9ddb3a097fc
Nathan Glick <nateleighglick@hotmail.com>
2020-10-27 08:20:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name) 
+  return "Hello, my name is #{name}."
+end
+  
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+  badge = badge_maker(name)
+  badges << badge
+ end
+ badges
+end 
+
+def assign_rooms(attendees)
+  rooms = []
+  
+end
