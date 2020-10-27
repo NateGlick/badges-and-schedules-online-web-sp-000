2303a46eb85b8aca0df17302c430cf824a7d6efe
Nathan Glick <nateleighglick@hotmail.com>
2020-10-27 08:42:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,7 @@ def assign_rooms(attendees)
  def printer(attendees)
    badges = batch_badge_creator(attendees)
    rooms = assign_rooms(attendees)
-   
+   badges.each do |badge|
+     
  end
 end
